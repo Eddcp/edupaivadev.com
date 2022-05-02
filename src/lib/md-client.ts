be232af5037f21b80d945ab13db6c89e0c9e9eb6
eddcp <eduardocspv@gmail.com>
2022-05-02 11:18:51 -0300

    Non null assertion - temporary to supress warning

--- src/lib/md-client.ts
+++ src/lib/md-client.ts
@@ -39,7 +39,7 @@ export function getAllPosts(fields: string[] = []) {
   const slugs = fs.readdirSync(postsDirectory)
   const posts = slugs
     .map((slug) => getPostBySlug(slug, fields))
-    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
+    .sort((post1, post2) => (post1!.date > post2!.date ? -1 : 1))
 
   return posts
 }
