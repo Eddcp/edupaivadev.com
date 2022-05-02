29096c3877a18d2f3686c3afe9ed4cac82e30320
eddcp <eduardocspv@gmail.com>
2022-05-02 12:42:52 -0300

    Fixing md-client

--- src/lib/md-client.ts
+++ src/lib/md-client.ts
@@ -5,17 +5,15 @@ import matter from 'gray-matter'
 const rootDirectory = process.cwd()
 const postsDirectory = join(rootDirectory, 'posts')
 
-export function getPostBySlug(slug: string, fields: string[] = []) {
-  if (!slug) return null
+type Items = {
+  [key: string]: string
+}
 
-  const realSlug = slug.replace('/.md$/', '')
-  const fullPath = join(postsDirectory, `${realSlug}`)
+export function getPostBySlug(slug: string, fields: string[] = []): Items {
+  const realSlug = slug.replace(/\.md$/, '')
+  const fullPath = join(postsDirectory, `${realSlug}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
   const { data, content } = matter(fileContents)
-
-  type Items = {
-    [key: string]: string
-  }
 
   const items: Items = {}
 
@@ -35,9 +33,10 @@ export function getPostBySlug(slug: string, fields: string[] = []) {
   return items
 }
 
-export function getAllPosts(fields: string[] = []) {
+export function getAllPosts(fields: string[] = []): Items[] {
   const slugs = fs.readdirSync(postsDirectory)
-  const posts = slugs.map((slug) => getPostBySlug(slug, fields))
-  // .sort((post1, post2) => (post1.date > post2.date ? -1 : 1)) sort later
+  const posts = slugs
+    .map((slug) => getPostBySlug(slug, fields))
+    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
   return posts
 }
