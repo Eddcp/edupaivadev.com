213c31f123fb3f616464c1aa2a174650efbf4a08
eddcp <eduardocspv@gmail.com>
2022-05-05 09:45:24 -0300

    Changing 3d logo image

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image'
 
 const HeroSection = () => (
   <>
-    <section className="py-20">
-      <div className="flex flex-col-reverse justify-between lg:flex-row">
+    <section className="py-20 fade-in-start">
+      <div className="flex flex-col-reverse justify-between lg:flex-row lg:mx-auto lg:w-[85%]">
         <div className="flex flex-col items-center text-left">
-          <h1 className="mt-20 text-3xl font-bold sm:text-5xl">
+          <h1 data-fade="1" className="text-3xl font-bold sm:text-5xl lg:mt-20">
             Desenvolvimento pessoal, profissional e financeiro.
           </h1>
-          <p className="mt-6 text-base text-white sm:text-lg">
+          <p data-fade="2" className="mt-6 text-base text-white sm:text-lg">
             Meu nome é Eduardo Paiva e atuo como desenvolvedor há mais de 5
             anos. Desde cedo na carreira percebi a ironia e dificuldade da
             classe de se desenvolver para além do campo técnico. Aqui você
@@ -20,8 +20,8 @@ const HeroSection = () => (
           <Image
             src="/img/3dlogo.png"
             alt="logo 3d"
-            width="2500"
-            height="2000"
+            width="2048"
+            height="2048"
           />
         </div>
       </div>
