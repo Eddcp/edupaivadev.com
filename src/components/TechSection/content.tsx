55fa8d3fac0085d9092a5f58f580287e4da4d422
eddcp <eduardocspv@gmail.com>
2022-05-11 02:06:37 -0300

    tech-stack flexbox

---
+++ src/components/TechSection/content.tsx
@@ -0,0 +1,28 @@
+const techStack = [
+  {
+    label: 'Java'
+  },
+  {
+    label: 'Spring'
+  },
+  {
+    label: 'Javascript'
+  },
+  {
+    label: 'Typescript'
+  },
+  {
+    label: 'ReactJS'
+  },
+  {
+    label: 'NextJS'
+  },
+  {
+    label: 'NodeJS'
+  },
+  {
+    label: 'AWS'
+  }
+]
+
+export default techStack
