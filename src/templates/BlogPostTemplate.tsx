2e18ed382f44606fee9bf46541c749b0b3594b67
eddcp <eduardocspv@gmail.com>
2022-05-23 21:39:43 -0300

    Adding next seo

--- src/templates/BlogPostTemplate.tsx
+++ src/templates/BlogPostTemplate.tsx
@@ -12,6 +12,7 @@ import CycleSvg from '@/../public/img/categories-icons/quebre-o-ciclo.svg'
 import CodeSvg from '@/../public/img/categories-icons/quebre-o-codigo.svg'
 import FightSvg from '@/../public/img/categories-icons/quebre-o-padrao.svg'
 import RoutineSvg from '@/../public/img/categories-icons/quebre-a-rotina.svg'
+import { NextSeo } from 'next-seo'
 
 type Component = {
   [key: string]: React.FunctionComponent<React.SVGProps<SVGPolygonElement>>
@@ -31,62 +32,77 @@ const BlogPostTemplate = ({ post }: BlogPost) => {
   const { width } = useWindowSize()
 
   return (
-    <section className="layout markdown">
-      <div className="grid grid-cols-12">
-        <article className="col-span-12 mt-10 max-w-[100%] lg:col-span-12">
-          <div className="flex justify-between">
-            <div>
-              {CategorySVG !== undefined ? (
-                <CategorySVG
-                  className={`border-[#FF0043] border-solid border-2 inline-block fill-current h-8 w-8`}
-                />
-              ) : (
-                ''
-              )}
-              <span className="ml-2">{post.categories}</span>
+    <>
+      <NextSeo
+        title={`${post.title} - edupaivadev`}
+        description={post.content}
+        openGraph={{
+          url: post.thumbnail,
+          title: `${post.title} - edupaivadev`,
+          images: [
+            {
+              url: post.thumbnail
+            }
+          ]
+        }}
+      />
+      <section className="layout markdown">
+        <div className="grid grid-cols-12">
+          <article className="col-span-12 mt-10 max-w-[100%] lg:col-span-12">
+            <div className="flex justify-between">
+              <div>
+                {CategorySVG !== undefined ? (
+                  <CategorySVG
+                    className={`border-[#FF0043] border-solid border-2 inline-block fill-current h-8 w-8`}
+                  />
+                ) : (
+                  ''
+                )}
+                <span className="ml-2">{post.categories}</span>
+              </div>
+              <div>{post.date}</div>
             </div>
-            <div>{post.date}</div>
-          </div>
-          <div className="relative mt-5 w-full">
-            <Image
-              src={post.thumbnail}
-              layout="responsive"
-              width={1000}
-              height={SizeHelper.isDesktop(width) ? 300 : 500}
-              objectFit="cover"
-              quality={75}
-            />
-          </div>
-          <section>
-            <h1 className="font-bold">{post.title}</h1>
-            <div className="flex justify-between mb-5">
-              <span className="font-antarctican text-sm font-bold uppercase lg:text-lg">
-                {post.author ? `por ${post.author}` : ''}
-              </span>
-              <span className="text-sm lg:text-lg">
-                {timeToRead(post.content)}
-              </span>
+            <div className="relative mt-5 w-full">
+              <Image
+                src={post.thumbnail}
+                layout="responsive"
+                width={1000}
+                height={SizeHelper.isDesktop(width) ? 300 : 500}
+                objectFit="cover"
+                quality={75}
+              />
             </div>
-            <hr className="h-0.5 bg-red border-none" />
-            <ReactMarkdown
-              components={{
-                code({ className, children }) {
-                  const language = className?.replace('language-', '')
-                  return (
-                    <SyntaxHighlighter style={darcula} language={language}>
-                      {String(children).replace(/\n$/, '')}
-                    </SyntaxHighlighter>
-                  )
-                }
-              }}
-            >
-              {post.content}
-            </ReactMarkdown>
-            <hr className="h-0.5 bg-red border-none" />
-          </section>
-        </article>
-      </div>
-    </section>
+            <section>
+              <h1 className="font-bold">{post.title}</h1>
+              <div className="flex justify-between mb-5">
+                <span className="font-antarctican text-sm font-bold uppercase lg:text-lg">
+                  {post.author ? `por ${post.author}` : ''}
+                </span>
+                <span className="text-sm lg:text-lg">
+                  {timeToRead(post.content)}
+                </span>
+              </div>
+              <hr className="h-0.5 bg-red border-none" />
+              <ReactMarkdown
+                components={{
+                  code({ className, children }) {
+                    const language = className?.replace('language-', '')
+                    return (
+                      <SyntaxHighlighter style={darcula} language={language}>
+                        {String(children).replace(/\n$/, '')}
+                      </SyntaxHighlighter>
+                    )
+                  }
+                }}
+              >
+                {post.content}
+              </ReactMarkdown>
+              <hr className="h-0.5 bg-red border-none" />
+            </section>
+          </article>
+        </div>
+      </section>
+    </>
   )
 }
 
