6ac4fa4d9092dec34e5bfc10ebb742548b013309
eddcp <eduardocspv@gmail.com>
2022-05-25 07:50:08 -0300

    Fix formatting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -33,7 +33,10 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>edupaivadev</title>
-        <meta name="google-site-verification" content="VuzlTcaE4VY0DB88TzhPBxkOjU21stn5lrQoZavoAhY" />
+        <meta
+          name="google-site-verification"
+          content="VuzlTcaE4VY0DB88TzhPBxkOjU21stn5lrQoZavoAhY"
+        />
         <link rel="shortcut icon" href="/img/avatar-meta-icon.png" />
         <link rel="apple-touch-icon" href="/img/avatar-meta-icon.png" />
         <meta
