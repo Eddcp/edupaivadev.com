5ae9f7e5d635e34e475053dc59a35ce051fe45d9
eddcp <eduardocspv@gmail.com>
2023-10-19 22:01:06 -0300

    EP-000 - Adding search feature

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,23 +1,57 @@
 import PostList from 'components/PostList'
 import { getAllPosts } from 'lib/md-client'
 import BlogPostItem from '@/types/post'
+import { useState } from 'react'
 
 type Props = {
   allPosts: BlogPostItem[]
 }
 
-const BlogPage = ({ allPosts }: Props) => (
-  <section className="layout">
-    <h1 className="text-3xl font-bold sm:mt-20 sm:text-5xl">Blog</h1>
-    <h2 className="mt-5 text-xl">
-      Pensamentos, ensinamentos e mais algumas coisas.
-    </h2>
-    <section className="mb-40">
-      <PostList posts={allPosts} className="grid grid-cols-3 gap-2 mt-4" />
+/*
+mt-4 w-full rounded-md dark:bg-dark border border-gray-300 dark:border-gray-600 
+focus:border-primary-300 focus:outline-none focus:ring-0 dark:focus:border-primary-300
+*/
+const BlogPage = ({ allPosts }: Props) => {
+  const [search, setSearch] = useState<string>('')
+
+  const searchFilter = (array: BlogPostItem[]): BlogPostItem[] => {
+    return array.filter((post: BlogPostItem) =>
+      post.title.toLowerCase().includes(search.toLowerCase())
+    )
+  }
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value)
+  }
+
+  const filteredPosts = searchFilter(allPosts)
+
+  return (
+    <section className="layout">
+      <h1 className="text-3xl font-bold sm:mt-20 sm:text-5xl">Blog</h1>
+      <h2 className="mt-5 text-xl">
+        Pensamentos, ensinamentos e mais algumas coisas.
+      </h2>
+      <div className="mt-5">
+        <input
+          type="text"
+          id="search"
+          name="search"
+          onChange={handleSearch}
+          className="peer px-2 w-full h-10 bg-transparent rounded-md border focus:border-red-300 focus:outline-none transition-colors"
+          placeholder="Search..."
+        />
+      </div>
+      <section className="mb-40">
+        <PostList
+          posts={filteredPosts}
+          className="grid grid-cols-3 gap-2 mt-4"
+        />
+      </section>
+      <hr className="h-0.5 bg-red border-none" />
     </section>
-    <hr className="h-0.5 bg-red border-none" />
-  </section>
-)
+  )
+}
 
 export const getStaticProps = async () => {
   const allPosts = getAllPosts([
